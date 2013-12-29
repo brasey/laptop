18bc211d48d09f9cfafb25bdcaa78cc275580154
Bob Rasey <bob.rasey@manheim.com>
2013-12-29 16:12:24 -0500

    Typo

--- pidgin/manifests/init.pp
+++ pidgin/manifests/init.pp
@@ -9,10 +9,10 @@ class pidgin {
   }
 
   exec { 'restore_pidgin_configs':
-    command   => "/usr/bin/scp -rp pi@10.0.0.18:pidgin/* /home/${::id}/.purple/",
-    user      => $::id,
-    requires  => Package[ 'pidgin' ],
-    onlyif    => '/usr/bin/grep -c prpl-sipe .purple/accounts.xml',
+    command => "/usr/bin/scp -rp pi@10.0.0.18:pidgin/* /home/${::id}/.purple/",
+    user    => $::id,
+    require => Package[ 'pidgin' ],
+    onlyif  => '/usr/bin/grep -c prpl-sipe .purple/accounts.xml',
   }
 
 }
--- thunderbird/manifests/init.pp
+++ thunderbird/manifests/init.pp
@@ -5,10 +5,10 @@ class thunderbird {
   }
 
   exec { 'restore_thunderbird':
-    command   => "/usr/bin/scp -rp pi@10.0.0.18:thunderbird/* /home/${::id}/.thunderbird/",
-    user      => $::id,
-    requires  => Package[ 'thunderbird' ],
-    onlyif    => '/usr/bin/grep -c gmail .thunderbird/*.default/prefs.js',
+    command => "/usr/bin/scp -rp pi@10.0.0.18:thunderbird/* /home/${::id}/.thunderbird/",
+    user    => $::id,
+    require => Package[ 'thunderbird' ],
+    onlyif  => '/usr/bin/grep -c gmail .thunderbird/*.default/prefs.js',
   }
 
 }
