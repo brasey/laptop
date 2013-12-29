80db2cf2c4e2a8f8bc92b9813ea020e202e05479
Bob Rasey <bob.rasey@manheim.com>
2013-12-29 16:11:15 -0500

    Typo

--- gpg/manifests/init.pp
+++ gpg/manifests/init.pp
@@ -5,10 +5,10 @@ class gpg {
   }
 
   exec { 'fetch_files':
-    command   => "/usr/bin/scp -rp pi@10.0.0.18:gpg/* /home/${::id}/.gnupg/",
-    user      => $::id,
-    requires  => Package[ 'gnupg' ],
-    onlyif    => '/usr/bin/gpg --list-secret-keys | /usr/bin/grep -c uid',
+    command => "/usr/bin/scp -rp pi@10.0.0.18:gpg/* /home/${::id}/.gnupg/",
+    user    => $::id,
+    require => Package[ 'gnupg' ],
+    onlyif  => '/usr/bin/gpg --list-secret-keys | /usr/bin/grep -c uid',
   }
 
 }
