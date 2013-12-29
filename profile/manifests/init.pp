5423d85ad295f3b85f1c7d0b39ea1f3f8fd2650c
Bob Rasey <bob.rasey@manheim.com>
2013-12-29 15:41:26 -0500

    Add profile

---
+++ profile/manifests/init.pp
@@ -0,0 +1,23 @@
+class profile {
+  include aliases
+  include backups
+  include bashgitprompt
+  include dropbox
+  include gimp
+  include gitg
+  include gitrepos
+  include gnometweaktool
+  include googlechrome
+  include gpg
+  include icedtea-web
+  include infinality
+  include keepassx
+  include manheimpuppetrepos
+  include pdfmerge
+  include pidgin
+  include profile
+  include sudoers
+  include thunderbird
+  include tig
+  include vim
+}
