232296c3fcd765bd1bb9cea6aab019b7553b2e29
Bob Rasey <bob.rasey@manheim.com>
2013-12-29 14:42:35 -0500

    Add thunderbird

---
+++ thunderbird/manifests/init.pp
@@ -0,0 +1,14 @@
+class thunderbird {
+
+  package { 'thunderbird':
+    ensure  => installed,
+  }
+
+  exec { 'restore_configs':
+    command   => "/usr/bin/scp -rp pi@10.0.0.18:thunderbird/* /home/${::id}/.thunderbird/",
+    user      => $::id,
+    requires  => Package[ 'thunderbird' ],
+    onlyif    => '/usr/bin/grep -c gmail .thunderbird/*.default/prefs.js',
+  }
+
+}
