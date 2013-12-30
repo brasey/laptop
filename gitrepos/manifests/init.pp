eb68a96071bfd18821bb0c9f8bfe9e5c0cfb419f
Bob Rasey <bob.rasey@manheim.com>
2013-12-30 09:14:40 -0500

    Remove git version from path

--- gitrepos/manifests/init.pp
+++ gitrepos/manifests/init.pp
@@ -15,12 +15,12 @@ class gitrepos {
 
   exec { 'make_gnome_keyring_connector':
     command => '/bin/make',
-    cwd     => '/usr/share/doc/git-1.8.3.1/contrib/credential/gnome-keyring',
+    cwd     => '/usr/share/doc/git/contrib/credential/gnome-keyring',
     user    => 'root',
   }
 
   exec { 'enable_gnome_keyring_connector':
-    command => '/usr/bin/git config --global credential.helper /usr/share/doc/git-1.8.3.1/contrib/credential/gnome-keyring/git-credential-gnome-keyring',
+    command => '/usr/bin/git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring',
     user    => $::id,
     onlyif  => "/usr/bin/grep -c gnome-keyring /home/${::id}/.gitconfig",
   }
