978280a3ef07c126520b86b9332729defe042ca3
Bob Rasey <bob.rasey@manheim.com>
2013-12-30 16:42:27 -0500

    Fix onlyif test

--- bashgitprompt/manifests/init.pp
+++ bashgitprompt/manifests/init.pp
@@ -1,9 +1,12 @@
 class bashgitprompt {
 
+  $gitbase = '/home/brasey/git'
+
   exec { 'fetch_repo':
     command => '/usr/bin/git clone https://github.com/magicmonty/bash-git-prompt.git',
-    cwd     => '/home/brasey/git',
+    cwd     => $gitbase,
     user    => 'brasey',
+    onlyif  => "/usr/bin/test ! -d ${gitbase}/bash-git-prompt",
     require => Class[ 'gitrepos' ],
   }
 
