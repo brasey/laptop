e2cab8807cd0d44b6d1650bd1c3c38f1ae34c945
Bob Rasey <bob.rasey@manheim.com>
2013-12-30 17:04:37 -0500

    Use file resource for git config

--- gitrepos/manifests/init.pp
+++ gitrepos/manifests/init.pp
@@ -23,16 +23,12 @@ class gitrepos {
     require     => Class[ 'gcc' ],
   }
 
-  exec { 'enable_gnome_keyring_connector':
-    command => '/usr/bin/git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring',
-    user    => 'brasey',
-    onlyif  => '/usr/bin/test "$( /usr/bin/grep -c gnome-keyring /home/brasey/.gitconfig )" != 0',
-  }
-
-  exec { 'enable_simple_push':
-    command => '/usr/bin/git config --global push.default simple',
-    user    => 'brasey',
-    onlyif  => '/usr/bin/test "$( /usr/bin/grep -c \'\[push\]\' /home/brasey/.gitconfig )" != 0',
+  file { '/home/brasey/.gitconfig':
+    ensure  => file,
+    owner   => 'brasey',
+    group   => 'brasey',
+    mode    => '0664',
+    source  => 'file:///etc/puppet/modules/gitrepos/files/.gitconfig',
   }
 
 }
