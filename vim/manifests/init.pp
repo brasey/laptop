ba9cf3bb16356efe40f0133294fe61488ac5d9c3
Bob Rasey <bob.rasey@manheim.com>
2014-01-02 12:59:19 -0500

    Solarize vim

--- vim/manifests/init.pp
+++ vim/manifests/init.pp
@@ -1,15 +1,45 @@
 class vim {
 
+  $solarized = 'yes'
+
+  File {
+      ensure  => file,
+      owner   => 'brasey',
+      group   => 'brasey',
+      mode    => '0664',
+  }
+
   package { 'vim-enhanced':
     ensure  => installed,
   }
 
-  file { '/home/brasey/.vimrc':
-    ensure  => file,
-    source  => 'file:///etc/puppet/modules/vim/files/.vimrc',
-    owner   => 'brasey',
-    group   => 'brasey',
-    mode    => '0664',
+  if $solarized == 'yes' {
+
+    $gitbase = '/home/brasey/git'
+
+    exec { 'clone_vim-colors-solarized_repo':
+      command => '/usr/bin/git clone https://github.com/altercation/vim-colors-solarized.git',
+      cwd     => $gitbase,
+      user    => 'brasey',
+      onlyif  => "/usr/bin/test ! -d ${gitbase}/vim-colors-solarized",
+      require => Class[ 'gitrepos' ],
+    }
+
+    file { '/home/brasey/.vimrc':
+      source  => 'file:///etc/puppet/modules/vim/files/.vimrc.solarized',
+    }
+
+    file { '/home/brasey/.vim/colors/solarized.vim':
+      source  => "file://${gitbase}/vim-colors-solarized/colors/solarized.vim"
+    }
+
+  }
+  else {
+
+    file { '/home/brasey/.vimrc':
+      source  => 'file:///etc/puppet/modules/vim/files/.vimrc',
+    }
+
   }
 
 }
