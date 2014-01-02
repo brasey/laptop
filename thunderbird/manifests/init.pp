79398847cacc0bc3c0fe0b9833715485b32c3ee6
Bob Rasey <bob.rasey@manheim.com>
2014-01-02 12:20:20 -0500

    Add profile dir resource

--- thunderbird/manifests/init.pp
+++ thunderbird/manifests/init.pp
@@ -1,14 +1,23 @@
 class thunderbird {
 
+  $profile_path = '/home/brasey/.thunderbird/hkoss3qt.default'
+
   package { 'thunderbird':
     ensure  => installed,
   }
 
+  file { $profile_path:
+    ensure  => directory,
+    owner   => 'brasey',
+    group   => 'brasey',
+    mode    => '0700',
+  }
+
   exec { 'restore_thunderbird':
-    command => '/usr/bin/scp -rp pi@10.0.0.18:thunderbird/* /home/brasey/.thunderbird/hkoss3qt/',
+    command => "/usr/bin/scp -rp pi@10.0.0.18:thunderbird/* ${profile_path}/",
     user    => 'brasey',
-    require => Package[ 'thunderbird' ],
-    onlyif  => '/usr/bin/grep -c gmail .thunderbird/*.default/prefs.js',
+    require => File[ $profile_path ],
+    onlyif  => "/usr/bin/grep -c gmail ${profile_path}/prefs.js",
   }
 
 }
