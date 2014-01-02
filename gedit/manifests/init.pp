701b0d4f360933331d2ea350b97fd25ca655a232
Bob Rasey <bob.rasey@manheim.com>
2014-01-02 13:45:06 -0500

    Add solarized gedit

---
+++ gedit/manifests/init.pp
@@ -0,0 +1,35 @@
+class gedit {
+
+  $gitbase = '/home/brasey/git'
+  $geditbase = '/home/brasey/.local/share/gedit'
+
+  File {
+    owner   => 'brasey',
+    group   => 'brasey',
+  }
+
+  package { 'gedit':
+    ensure  => intsalled,
+  }
+
+  exec { 'clone_solarized-gedit_repo':
+    command => '/usr/bin/git clone https://github.com/mattcan/solarized-gedit.git',
+    cwd     => $gitbase,
+    user    => 'brasey',
+    onlyif  => "/usr/bin/test ! -d ${gitbase}/solarized-gedit",
+    require => Class[ 'gitrepos' ],
+  }
+
+  file { [ $geditbase, "${geditbase}/styles" ]:
+    ensure  => directory,
+    mode    => '0775',
+  }
+
+  file { "${geditbase}/styles/solarized-dark.xml":
+    ensure  => file,
+    mode    => '0664',
+    source  => "file://${gitbase}/solarized-gedit/solarized-dark.xml",
+    require => [ File[ "${geditbase}/styles" ], Exec[ 'clone_solarized-gedit_repo' ] ],
+  }
+
+}
