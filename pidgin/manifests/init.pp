2b487ed2f9e70f9cbde62c06685c6f049df6f68c
Bob Rasey <bob.rasey@manheim.com>
2014-01-02 16:53:25 -0500

    Change onlyif test

--- pidgin/manifests/init.pp
+++ pidgin/manifests/init.pp
@@ -12,7 +12,7 @@ class pidgin {
     command => '/usr/bin/rsync -ave ssh pi@10.0.0.18:pidgin/ /home/brasey/.purple/',
     user    => 'brasey',
     require => Package[ 'pidgin' ],
-    onlyif  => '/usr/bin/grep -c prpl-sipe .purple/accounts.xml',
+    onlyif  => '/usr/bin/test "$(/usr/bin/grep -c prpl-sipe .purple/accounts.xml)" = 0',
   }
 
 }
