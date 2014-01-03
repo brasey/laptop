7d478ac8b3f32c3ccf90cdaa0aea1a33b887d040
Bob Rasey <bob.rasey@manheim.com>
2014-01-02 19:49:44 -0500

    Change onlyif test

--- gpg/manifests/init.pp
+++ gpg/manifests/init.pp
@@ -8,7 +8,7 @@ class gpg {
     command => '/usr/bin/rsync -ave ssh pi@10.0.0.18:gpg/ /home/brasey/.gnupg/',
     user    => 'brasey',
     require => Package[ 'gnupg' ],
-    onlyif  => '/usr/bin/gpg --list-secret-keys | /usr/bin/grep -c uid',
+    onlyif  => '/usr/bin/test "$(/usr/bin/gpg --list-secret-keys | /usr/bin/grep -c uid)" = 0',
   }
 
 }
