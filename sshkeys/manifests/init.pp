98d3831b6f059c236b3fa0bf4621d2d0fce7d9e3
Bob Rasey <bob.rasey@manheim.com>
2014-01-02 20:29:55 -0500

    Change onlif test

--- sshkeys/manifests/init.pp
+++ sshkeys/manifests/init.pp
@@ -3,7 +3,7 @@ class sshkeys {
   exec { 'restore_sshkeys':
     command => '/usr/bin/rsync -ave ssh pi@10.0.0.18:ssh/ /home/brasey/.ssh',
     user    => 'brasey',
-    onlyif  => '/usr/bin/test ! -f /home/brasey/.ssh/id_rsa',
+    onlyif  => '/usr/bin/test ! -f /home/brasey/.ssh/id_rsa_manheim',
   }
 
 }
