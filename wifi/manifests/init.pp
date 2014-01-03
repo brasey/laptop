53c74b270646497f908d677ccb6896d5ad47755c
Bob Rasey <bob.rasey@manheim.com>
2014-01-03 08:20:39 -0500

    Remove wifi

--- wifi/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class wifi {
-
-  exec { 'restore_wifi':
-    command => '/usr/bin/rsync -ave ssh pi@10.0.0.18:wifi/ /etc/sysconfig/network-scripts/',
-    user    => 'root',
-    onlyif  => '/usr/bin/test ! -f /etc/sysconfig/network-scripts/ifcfg-Auto_rasey',
-  }
-
-}
