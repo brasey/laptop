515b25fb2f7435ebb5eb6b434df154a28c33b52c
Bob Rasey <bob.rasey@manheim.com>
2014-01-02 19:44:50 -0500

    Change onlyif test

--- seahorse/manifests/init.pp
+++ seahorse/manifests/init.pp
@@ -8,7 +8,7 @@ class seahorse {
     command => '/usr/bin/rsync -ave ssh pi@10.0.0.18:keyrings/ /home/brasey/.local/share/keyrings/',
     user    => 'brasey',
     require => Package[ 'seahorse' ],
-    onlyif  => '/usr/bin/test ! -f /home/brasey/.local/share/keyrings/login.keyring',
+    onlyif  => '/usr/bin/test ! -f /home/brasey/.local/share/keyrings/user.keystore.60AC2W',
   }
 
 }
