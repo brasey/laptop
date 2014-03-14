cddc35faee31f999d8381d6dd5b4f936c345c42e
Bob Rasey <bob.rasey@manheim.com>
2014-03-14 11:37:24 -0400

    Add todo.txt

---
+++ todotxt/manifests/init.pp
@@ -0,0 +1,13 @@
+class todotxt {
+
+  $gitbase = '/home/brasey/git'
+
+  exec { 'todo.txt_repo':
+    command => '/usr/bin/git clone https://github.com/ginatrapani/todo.txt-cli.git',
+    cwd     => $gitbase,
+    user    => 'brasey',
+    onlyif  => "/usr/bin/test ! -d ${gitbase}/todo.txt-cli",
+    require => Class[ 'gitrepos' ],
+  }
+
+}
